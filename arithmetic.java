c5e5f325a2ca2404298f24b6a2b9101c9cd930b2
mprashanth634-jpg <mprashanth634@gmail.com>
2026-08-24 17:04:47 +0530

    Initial Commit

---
+++ arithmetic.java
@@ -0,0 +1,18 @@
+public class arithmetic {
+    public static void main(String args[]) {
+        System.out.println("This is Artithmetic operators");
+
+        System.out.println("////////////////////////////////////////////////" + "\n");
+
+        System.out.println("The symbols are: ");
+
+        System.out.println("Addition: " + "+");
+        System.out.println("Subtraction: " + "-");
+
+        System.out.println("////////////////////////////////////////////////" + "\n");
+
+        System.out.println("Example Operations: ");
+
+        System.out.println("Additon: " + (10 + 10));
+    }
+}
